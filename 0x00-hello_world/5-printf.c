7dd8626cfa5f1e4d4b90371f25f44b3bf73f3e5f
iamcirin <cirine7xu@gmail.com>
2023-02-16 04:42:06 -0800

    added file 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
 
-int main(void){
+/* func that prints a phrase and return 0*/
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;}
+	return 0;
+}
