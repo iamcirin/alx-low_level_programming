c235ec1a2643e3bb6d49cdd758a1b8c410acc1f4
iamcirin <cirine7xu@gmail.com>
2023-02-16 07:31:14 -0800

    added file 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+/**
+ * main - Entry point
+ *
+ * Description: a program that prints the sizeof different types. 
+ *
+ *Return: returns 0
+ */
+
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)", sizeof(char));
+	printf("Size of an int: %lu byte(s)", sizeof(int));
+	printf("Size of a long int: %lu byte(s)", sizeof(long int));
+	
+	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)", sizeof(float));
+
+	return (0);
+
+}
+
+
