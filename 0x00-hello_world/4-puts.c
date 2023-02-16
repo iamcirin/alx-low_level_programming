4a0e1fc628e64ab1567c5adc008d741564e212c2
iamcirin <cirine7xu@gmail.com>
2023-02-16 07:03:21 -0800

    added file 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/*
+ *
+ * main - Entry point
+ *
+ * Description: a function that prints using puts.
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	char str[] = "Programming is like building a multilingual puzzle";
+
+		puts(str);
+
+	return 0;
+}
+
