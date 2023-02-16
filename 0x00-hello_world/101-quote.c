cd182ac5fa6727b6cbfe4937387a05e7e0144d8f
iamcirin <cirine7xu@gmail.com>
2023-02-16 09:00:27 -0800

    added file 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/*
+ * main - Entry point
+ * Description: program that writes a text
+ * Return: return 1
+ */
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19";
+	write(1, quo, 59);
+	return (1);
+
+}
