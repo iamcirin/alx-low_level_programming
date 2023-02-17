e653bdf8df829a91662d20637723ba09e851b73c
iamcirin <cirine7xu@gmail.com>
2023-02-17 07:46:14 -0800

    added file 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: checks if the last digit of n
+ *
+ * Return: returns 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+	/* your code goes there */
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		putchar('\n');
+		letter++;
+	}
+	return (0);
+}
