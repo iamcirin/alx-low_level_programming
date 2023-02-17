23923d363b0076be869ecc9c245e88992b7e8859
iamcirin <cirine7xu@gmail.com>
2023-02-17 08:51:40 -0800

    added file 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints single digit numbers of vase 10
+ *
+ * Return: returns 0 (Success)
+ */
+int main(void)
+{
+	/* your code goes there */
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
