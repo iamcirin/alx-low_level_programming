9ddb2533063ff86280327809bcfd031177d3d6fd
iamcirin <cirine7xu@gmail.com>
2023-02-17 10:29:37 -0800

    added file 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,17 +15,17 @@ int main(void)
 	char i;
 	char j;
 
-	for (i = '0'; i <= '8'; i++)
+	for (i = 0; i <= 8; i++)
 	{
-		for (j = i + 1; j <= '9'; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
-		putchar(i);
-		putchar(j);
-		if (i != '8' && j != '9')
+		putchar(i + '0');
+		putchar(j + '0');
+		if (i < 8 || j < 9)
 		{
-		}
 		putchar(',');
 		putchar(' ');
+		}
 		}
 	}
 	putchar('\n');
