d66b0847f104d329922779fc4643381c44cfdd28
iamcirin <cirine7xu@gmail.com>
2023-02-21 03:39:14 -0800

    added file 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar using putchar func inside a loop.
+ *
+ * Return: return 0 (Success)
+ */
+
+int main(void)
+{
+	char word[] = "_putchar";
+	int i = 0;
+
+	while (i < 8)
+	{
+		putchar(word[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
