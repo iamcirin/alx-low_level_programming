758c941bde36cecc7bebb441eda024af6c45c7af
iamcirin <cirine7xu@gmail.com>
2023-02-21 16:31:56 -0800

    added file 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include"main.h"
+
+/**
+ * times_table - print last digit of a number
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int prod;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(48);
+		for (j = 1; j <= 9; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = i * j;
+
+			if (prod <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((prod / 10) + 48);
+			}
+			_putchar((prod % 10) + 48);
+			
+		}
+			_putchar('\n');
+	}
+
+}
