f1fb2a48453d1f229d5390fd69f5b73ee35bef63
iamcirin <cirine7xu@gmail.com>
2023-02-21 16:11:47 -0800

    added file 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include"main.h"
+
+/**
+ * jack_bauer - print last digit of a number
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 23; i++)
+	{
+		for (j = 0; j <= 59; j++)
+		{
+			_putchar((i / 10) + 48);
+			_putchar((i % 10) + 48);
+			_putchar(':');
+			_putchar((j / 10) + 48);
+			_putchar((j % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
