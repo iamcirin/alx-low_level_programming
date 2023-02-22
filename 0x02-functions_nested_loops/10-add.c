4f303a600dfc833f079673726699e2ebc749b4de
iamcirin <cirine7xu@gmail.com>
2023-02-21 16:54:51 -0800

    added file 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * add - print the sum of two integers
+ *
+ * @n: first input
+ * @m: second input
+ *
+ * Return: returns the result
+ */
+
+int add(int n, int m)
+{
+	int result;
+
+	result = n + m;
+
+	return (result);
+}
