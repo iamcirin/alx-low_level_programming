d8016c6fc4bac9d835ac07faadbefbda9d3e0f74
iamcirin <cirine7xu@gmail.com>
2023-02-27 14:39:43 -0800

    added file 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - Entry point
+ * @str: pointer param
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int len;
+
+	len = strlen(str);
+
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
