c6537897f66b45714e25479f3f10b8359765747b
iamcirin <cirine7xu@gmail.com>
2023-03-01 10:47:27 -0800

    added file 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - Entry point
+* @dest: pointer
+* @src: pointer
+* Return: returns a char array
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int i,j;
+	i = j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
