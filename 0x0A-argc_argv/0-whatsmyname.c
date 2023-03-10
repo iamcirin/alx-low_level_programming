1357e4796e862fea85eb09f45b43c48b448e3116
iamcirin <cirine7xu@gmail.com>
2023-03-10 14:24:31 -0800

    added file 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main(int argc, char* argv[])
+/**
+ * main - Entry point
+ * @argc: param
+ * @argv: param
+ * Return: returns success
+ */
+
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[argc - argc]);
 	return (0);
