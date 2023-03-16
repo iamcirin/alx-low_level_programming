7d0ebf544c80b9124477091f59d0162fb71a34b5
iamcirin <cirine7xu@gmail.com>
2023-03-16 12:36:17 -0700

    added file 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _calloc - Entry point
+ * @nmemb: param
+ * @size: param
+ * Return: returns p or Null
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		p[i] = 0;
+	}
+	return (p);
+}
+
