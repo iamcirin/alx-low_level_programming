bd1291f9af120bd48f964e2510ec823e557a8a15
iamcirin <cirine7xu@gmail.com>
2023-03-22 16:04:58 -0700

    added file 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function
+ * @array: param
+ * @size: param
+ * @action: pointer to function
+ */
+
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size, i++)
+	{
+		action(array[i]);
+
+	}
+}
