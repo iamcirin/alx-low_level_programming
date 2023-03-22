301846d3d55f0286442285ceb580bcde2a5244a5
iamcirin <cirine7xu@gmail.com>
2023-03-22 16:59:06 -0700

    added files 3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "cal.h"
+#include <stddef.h>
+
+/**
+ * get_op_func - pointer to function
+ * @s: param
+ * Return: returns int
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i;
+
+	while (i < 5)
+	{
+		if (*s == *ops[i].op)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include "function_pointers.h"
+
+/**
+ * op_add - adds 2 int
+ * @a: param
+ * @b: param
+ * Return: returns int
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sub of 2 int
+ * @a: param
+ * @b: param
+ * Return: returns int
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - product of  2 int
+ * @a: param
+ * @b: param
+ * Return: returns int
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * op_div - divides 2 int
+ * @a: param
+ * @b: param
+ * Return: returns int
+ */
+
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - reminder of 2 int
+ * @a: param
+ * @b: param
+ * Return: returns int
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
