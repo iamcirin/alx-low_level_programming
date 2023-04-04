527897b2e190ee738f52491ad83490c7a1b9bddc
iamcirin <cirine7xu@gmail.com>
2023-04-04 07:07:07 -0700

    update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,13 +1,20 @@
-#include<main.h>
+#include"main.h"
+
+/**
+ * print_times_table - prints times n table
+ * @n: param
+ */
 
 void print_times_table(int n)
 {
 	int prod;
+	int i;
+	int j;
 
-	for (int i = 0; i < n + 1; i++)
+	for (i = 0; i < n + 1; i++)
 	{
 		putchar(48);
-		for (int j = 1; j < n + 1; j++)
+		for (j = 1; j < n + 1; j++)
 		{
 		putchar(',');
 		putchar(' ');
@@ -21,16 +28,23 @@ void print_times_table(int n)
 		}
 		else if (prod > 9 && prod < 100)
 		{
-			
-			putchar(' ');
+
+		putchar(' ');
 			putchar((prod / 10) + 49);
 		}
-		else
+		else if (prod >= 100 && prod < 1000)
 		{
 
 			putchar((prod / 100) + 48);
-			putchar(((prod / 10) -10) + 48);
+			putchar(((prod / 10) - 10) + 48);
 		}
+		else
+		{
+			putchar((prod / 1000) + 48);
+			putchar(((prod / 100) % 10) + 48);
+			putchar(((prod / 10) % 10) + 48);
+		}
+
 		putchar(prod % 10 + 48);
 		}
 		putchar('\n');
