c21b16d1d2b8c4827e0e044a1f04d725cc21589c
iamcirin <cirine7xu@gmail.com>
2023-04-06 14:45:31 -0700

    added file 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the first node in the list
+ * Return: returns the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t = number = 1;
+
+	h == NULL && return (0);
+
+	while (*h.next != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] %S\n", "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		h = h->next;
+
+		number++;
+	}
+
+	printf("[%d] %s\n", h->len, h->str);
+	return (number);
+}
