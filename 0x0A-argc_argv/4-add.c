9acbdf83589508cedc893562a73050d21eb36f91
iamcirin <cirine7xu@gmail.com>
2023-06-12 03:56:15 -0700

    added file 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * main - a program that adds positive numbers
+ * @argc: returns the numnbers of the elements passed
+ * @argv: an array of the elements passed
+ * Return: returns 1 if on of the elements passed not a digit and 0 if success
+ */
+int main(int argc, char *argv[])
+
+{
+	int i, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (!isdigit(argv[i][0]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	for (i = 1; i < argc; i++)
+	{
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
