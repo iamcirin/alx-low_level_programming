eef8ab57339832135910d1a1e28a79c8cff99660
iamcirin <cirine7xu@gmail.com>
2023-06-12 02:22:19 -0700

    create 0-whatsmyname file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - a program that prints its name, followed by a new line.
+ * @argc: returns the number of elements passed to the CLI
+ * @argv: an array of pointers to char of the element passed
+ * Return: returns 0 - success
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	int i = 0;
+
+	printf("%s\n", argv[i]);
+	return (0);
+}
+
