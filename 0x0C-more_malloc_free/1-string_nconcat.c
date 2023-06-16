c5bb00b4a51eddbe19b0ae770fea2e4d2fc55f9e
iamcirin <cirine7xu@gmail.com>
2023-06-16 15:07:23 -0700

    modified file 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,24 +20,23 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 
 
-	while (*s1 != '\0')
+	while (s1[i] != '\0')
 	{
 		i++;
-		s1++;
 	}
 	while (s2[j] != '\0')
 	{
 		j++;
 	}
 
-	if (n > j)
+	if (n >= j)
 		n = j;
 
 	p = malloc(i + n + 1);
 	if (p == NULL)
 		return (NULL);
 
-	for (c = 0; c < i; c++)
+	for (c = 0; s1[c] != '\0'; c++)
 	{
 		p[c] = s1[c];
 	}
