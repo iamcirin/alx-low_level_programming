8175810e7ae8eb894bf4c0d8c8470ecd65c0bac4
iamcirin <cirine7xu@gmail.com>
2023-06-20 12:38:51 -0700

    added file 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - an Entry point
+ * Description: a program that prints the name of the current file
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
