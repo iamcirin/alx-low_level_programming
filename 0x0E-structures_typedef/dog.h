fe84cc7711ef720f536f7c2fca3a2f496db2511d
iamcirin <cirine7xu@gmail.com>
2023-06-21 06:51:49 -0700

    added file dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure named dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
