e1886568ba59284bc6613b541944b7b3eb4e28d1
iamcirin <cirine7xu@gmail.com>
2023-06-21 12:46:08 -0700

    added file 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -32,7 +32,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->owner = malloc(strlen(owner) + 1);
 	if (new_dog->owner == NULL)
 	{
-		free(dog->name);
+		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
