de49178589b38676303a50ca4c7993602a1d13d9
iamcirin <cirine7xu@gmail.com>
2023-06-23 10:43:04 -0700

    added file 3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,5 +8,20 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+/**
+ * struct print_all_args - a struct that holds 2 members
+ * @placeholder: a char represent the placeholder for
+ * the corresponding data type
+ * @func: a pointer to a function that prints the data type
+ * corresponding to the placeholder char
+ */
+
+typedef struct print_all_args
+{
+	char *placeholder;
+	void (*func)(va_list args);
+} print_arg;
 
 #endif
