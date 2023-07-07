311fc2781a357fb7744b8f948368078ca328f00c
iamcirin <cirine7xu@gmail.com>
2023-07-07 14:11:14 -0700

    add file 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * clear_bit - a function that sets the value a bit 0
+ * @n: an int
+ * @index: an int
+ * Return: returns 1 or -1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int m = 1;
+
+	if (index > 64)
+		return (-1);
+
+	m = ~(m << index);
+
+	*n = (*n & m);
+
+	return (-1);
+}
