39eb216515d03ff551c95cea9890cb6863aae6b6
iamcirin <cirine7xu@gmail.com>
2023-10-02 16:39:06 -0700

    added 0-whatsmyname.c & main.h files

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+ * main - The entry point
+ * @argc: the number of the command line arguments
+ * @argv: an array of the command line arguments
+ * Return: returns 0- success
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
