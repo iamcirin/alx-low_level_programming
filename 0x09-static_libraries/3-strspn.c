314aa978f0bf928c107f2fb940d5819597e9d305
iamcirin <cirine7xu@gmail.com>
2023-10-05 15:36:07 -0700

    add libmy.a file and main.h file

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,13 @@
+/**
+ * _strspn - a function that gets the
+ *           length of a prexif substring
+ *
+ * @s: pointer to string input
+ * @accept: substring prefix to look for
+ *
+ * Return: the number of bytes in the initial segment
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * _abs - function that computes the absolute
+ *       value of an integer
+ *
+ * @i: take input valaues for the function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int _abs(int i)
+{
+}
