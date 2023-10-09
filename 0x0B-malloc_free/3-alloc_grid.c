ebf3cb9fdfc72c442f79003fe6f28df0f448a23c
iamcirin <cirine7xu@gmail.com>
2023-10-09 13:51:38 -0700

    added file 4-free_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,6 +13,8 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	ptr = malloc(height * sizeof(int *));
+	if (ptr == NULL)
+		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * free_grid - a function that frees up a 2d array
+ * @grid: a pointer to pointer int
+ * @height: the number of 1-D arrays
+ */
+
+void free_grid(int **grid, int height)
+{
+	if (grid == NULL || height == 0)
+		return (NULL);
+
+	while (height >= 0)
+	{
+		free(grid[height]);
+		height--;
+	}
+	free(grid);
+}
