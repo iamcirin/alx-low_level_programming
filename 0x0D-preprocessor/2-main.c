19fe327e1697df8e761fa4ff2c6ca3f9a3fa8016
iamcirin <cirine7xu@gmail.com>
2023-10-10 13:18:11 -0700

    added 2-main.c & 3-function_like_macro.h files

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: a function that prints the file name
+ * compiled from with a new line
+ *
+ * Return: return a 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
