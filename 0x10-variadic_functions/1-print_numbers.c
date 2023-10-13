31fd75ae5b6b9d044bf2fcaa76a01e217b695c80
iamcirin <cirine7xu@gmail.com>
2023-10-13 11:13:44 -0700

    updated 1-print_numbers.c file

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -22,7 +22,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			printf("%d", x);
 			if (separator)
 			{
-				printf("%c ", *separator);
+				printf("%s", separator);
 			}
 
 		}
