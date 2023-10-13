f993deb3f4c75d334ec2b98bf179ee887a73259f
iamcirin <cirine7xu@gmail.com>
2023-10-13 11:09:37 -0700

    added 1-print_numbers.c file

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - a function that prints numbers followed by a sep
+ * @separator: a pointer to char
+ * @n: number of cars passed to the function
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list numbers;
+
+	unsigned int i;
+	int x;
+
+	va_start(numbers, n);
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(numbers, int);
+		if (i < n - 1)
+		{
+			printf("%d", x);
+			if (separator)
+			{
+				printf("%c ", *separator);
+			}
+
+		}
+		else
+		{
+			printf("%d\n", x);
+		}
+	}
+	va_end(numbers);
+}
+
